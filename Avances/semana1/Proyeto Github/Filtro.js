c34c4827cbf078987a32c74e2c19d1720acce5db
W610-PC30 <labinf@PMAWSW6LC0930.duoc.acad>
2025-04-16 12:37:48 -0400

    subiendo avances de la semana 1

---
+++ Avances/semana1/Proyeto Github/Filtro.js
@@ -0,0 +1,10 @@
+// script.js document.getElementById("search").addEventListener("input", function() { let searchTerm = this.value.toLowerCase(); let gameCards = document.querySelectorAll(".game-card");
+
+gameCards.forEach(function(card) {
+    let gameName = card.querySelector("h3").textContent.toLowerCase();
+    if (gameName.includes(searchTerm)) {
+        card.style.display = "block";
+    } else {
+        card.style.display = "none";
+    }
+});
